152dc40bbce1711ebc10b535342ae2cfd59af053
sreeragps <sreerag@brainchild.services>
2021-06-05 00:27:27 +0530

    changed index module

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    var q = url.parse(req.url);
+    var x=q.pathname;
+    if(x=="/welcome"){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('Hello World!');
+        res.end('');
+    }else if(x=="/contact"){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+          }));
+        res.end('');
+    }else{
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.end('');
+    }    
 }
-
+httpServer.listen(8081);
 module.exports = httpServer;
