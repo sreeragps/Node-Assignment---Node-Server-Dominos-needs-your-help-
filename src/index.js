c0243ee8d9151767a04e7fe734c10faf600aacbe
Sreerag P s <69072260+sreeragps@users.noreply.github.com>
2021-06-22 17:41:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function handleServer(req, res) {
     var x=q.pathname;
     if(x=="/welcome"){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('Hello World!');
+        res.write('Welcome to Dominos!');
         res.end('');
     }else if(x=="/contact"){
         res.writeHead(200, {'Content-Type': 'application/json'});
